564d786a9e573db733a26b3db5af3769e4e6eb4f
bastianawischus <Learn2Code2021>
2022-07-03 21:55:56 -0700

    added remaining dropdown options

--- cl_vars.py
+++ cl_vars.py
@@ -5,5 +5,12 @@ car_to_find = "Lexus GX470"
 # Buttons
 owner_btn = "//*[@id='owner-purveyor']"
 # Dropdown
-lowest_to_highest = "//*[@title='sort by price, lowest to highest']"
+
 drp_down = "//*[@data-selection='date']//../.."
+lowest_to_highest = "//*[@title='sort by price, lowest to highest']"
+highest_to_lowest = "//*[@title='sort by price, highest to lowest']"
+most_relevant = "//*[@title='show most relevant matches first']"
+newest_match = "//*[@title='show newest matches first']"
+
+
+
