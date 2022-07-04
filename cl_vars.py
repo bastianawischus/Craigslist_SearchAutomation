5b6efcd0162502b962c581e9865d8946fca0be2b
bastianawischus <Learn2Code2021>
2022-07-03 21:52:38 -0700

    Got sort by working

--- cl_vars.py
+++ cl_vars.py
@@ -5,6 +5,5 @@ car_to_find = "Lexus GX470"
 # Buttons
 owner_btn = "//*[@id='owner-purveyor']"
 # Dropdown
-sort_dropdown = "//*[@class='dropdown-list']"
 lowest_to_highest = "//*[@title='sort by price, lowest to highest']"
-lth = "//*[@data-selection='priceasc']"
+drp_down = "//*[@data-selection='date']//../.."
--- main.py
+++ main.py
@@ -14,9 +14,12 @@ def get_craigslist(cl_site):
     driver.get(cl_site)
     driver.find_element(By.XPATH, cl_vars.search_box).send_keys(cl_vars.car_to_find + Keys.ENTER)
     driver.find_element(By.XPATH, cl_vars.owner_btn).click()
-    driver.find_element(By.XPATH, cl_vars.lth).click()
-    # driver.implicitly_wait(10)
-    driver.quit()
+    driver.find_element(By.XPATH, cl_vars.drp_down).click()
+    driver.find_element(By.XPATH, cl_vars.lowest_to_highest).click()
+
+
 
 
 get_craigslist(url.cl_inland_empire_auto_search)
+
+driver.quit()
